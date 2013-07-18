8818c3894e9f7c3feb7013248993b5f906d9724f
renan <renan>
2013-07-18 16:29:15 +0000

    git-svn-id: http://svn.brlight.net/svn/lightbase-neo/trunk/LBApp@326 29b92fdf-8c97-4584-b987-84e8d3c556fa

--- lbapp/__init__.py
+++ lbapp/__init__.py
@@ -7,6 +7,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('pyramid', '/')
+	#11111
     config.add_route('home', 'home')
     config.add_route('teste', 'teste')
     config.add_route('criabase', 'criabase')
