29fa5a4492e5839eff41ce339f2d30115011ee4c
antony <antony>
2013-08-21 14:17:01 +0000

    organize
    git-svn-id: http://svn.brlight.net/svn/lightbase-neo/trunk/LBApp@472 29b92fdf-8c97-4584-b987-84e8d3c556fa

--- lbapp/__init__.py
+++ lbapp/__init__.py
@@ -6,6 +6,10 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
+
+    # BASE
+
+    config.add_route('edit_base', '{base_name}/edit')
     config.add_route('listarbase', 'listarbase')
     config.add_route('criarbase', 'criarbase')
     config.add_route('blank', 'blank')
--- lbapp/views.py
+++ lbapp/views.py
@@ -1,5 +1,10 @@
 from pyramid.view import view_config
 
+@view_config(route_name='edit_base', renderer='templates/base/edit.pt')
+def edit_base(request):
+    
+    
+    return {'json_base': 'json_base'}
 @view_config(route_name='listarbase', renderer='templates/listarbase.pt')
 def temp1(request):
     return {'project': 'LBApp'}
