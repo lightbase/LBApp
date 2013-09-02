c8708ce3e4803eeeb88ca614d73ff3f1fd7d36c0
renan <renan>
2013-09-02 21:03:28 +0000

    renderizando atributos da base (cor, nome, id, etc...)
    git-svn-id: http://svn.brlight.net/svn/lightbase-neo/trunk/LBApp@512 29b92fdf-8c97-4584-b987-84e8d3c556fa

--- lbapp/static/js/lightbase/base_onhover.js
+++ lbapp/static/js/lightbase/base_onhover.js
@@ -1,8 +1,89 @@
+var json = {"base": [{"color": "#006600"}, {"primeirabase":[{"nome_base": "base1"},{"id_base": "1"}], "segundabase": [{"nome_base": "base2"},{"id_base": "2"}]}]};
 var base_fast_menu = document.getElementById("base-fast-menu");
     //base_data = JSON.parse(document.getElementById("controller-base")),
     //linktobase = new linkToBase();
 
-function linkToBase(){
+function Header(base_fast_menu){
+
+    this.linkToBase = function(base_fast_menu){
+
+        var li = document.createElement("li"),
+            a = document.createElement("a"),
+            i = document.createElement("i");
+
+        li.setAttribute('id', 'fast_link_li_1');
+        li.setAttribute('style', 'background-color:' + json.base[0].color);
+        a.setAttribute('data-toggle', 'dropdown');
+        //a.setAttribute('class', 'dropdown-toggle');
+        a.setAttribute('href', '#');
+        a.setAttribute('id', 'a');
+        i.setAttribute('class', 'icon-th-list icon-animated-vertical');
+        i.setAttribute('style', 'width: 50px;');
+
+        a.appendChild(i);
+        li.appendChild(a);
+        base_fast_menu.appendChild(li);
+
+        this.html = li;
+
+        $('#fast_link_li_1').hover(function(){
+            $('#a').dropdown('toggle');
+        });
+
+    }
+
+    this.linkToBase_onHover = function(base_fast_menu){
+        var li_div_to_append = document.getElementById("fast_link_li_1"),
+            li = document.createElement("li"),
+            ul = document.createElement("ul");
+
+        ul.setAttribute('class', 'pull-right dropdown-navbar dropdown-menu dropdown-caret dropdown-closer');
+        ul.setAttribute('id', 'fast_link_ul_1');
+        li.setAttribute('class', 'nav-header');
+
+        li.innerHTML = JSON.stringify(json.base[1]);
+        ul.appendChild(li);
+        li_div_to_append.appendChild(ul);
+
+        this.html = li;
+
+    }
+
+    this.linkToBase_onHover_Info = function(base_fast_menu){
+        var ul = document.getElementById("fast_link_ul_1"),
+            li = document.createElement("li"),
+            a = document.createElement("a"),
+            img = document.createElement("img"),
+            span1 = document.createElement("span"),
+            span2 = document.createElement("span"),
+            span3 = document.createElement("span"),
+            i = document.createElement("i");
+
+            span1.setAttribute('class', 'blue');
+            span1.innerHTML = 'Alex:';
+            span2.setAttribute('class' ,'msg-title');
+            span3.setAttribute('class', 'msg-body');
+            img.setAttribute('class', 'msg-photo');
+            img.setAttribute('src', '/static/ace-final/assets/avatars/avatar.png');
+            img.setAttribute('alt', 'Alex Avatar');
+
+            span2.appendChild(span1);
+            span3.appendChild(span2);
+            a.appendChild(span2);
+            a.appendChild(img);
+            li.appendChild(a);
+            ul.appendChild(li);
+
+            this.html = li;
+
+    }
+
+
+}
+
+header = new Header(base_fast_menu);
+
+/*function linkToBase(){
     var li = document.createElement("li"),
         a = document.createElement("a"),
         i = document.createElement("i");
@@ -76,4 +157,4 @@ var ul = document.getElementById("fast_link_ul_1"),
 
 linktobase = new linkToBase();
 linkToBase_onhover = new linkToBase_onHover();
-linkToBase_onhover_info = new linkToBase_onHover_Info();
+linkToBase_onhover_info = new linkToBase_onHover_Info();*/
