46e5310e3c72ebbbcbfe567a03d76ffb5e5cc4df
Alessandro Landim <alessandrolandim@yahoo.com.br>
2015-03-23 18:14:26 -0300

    ajuste na variavel do heroku

--- lbapp/config/__init__.py
+++ lbapp/config/__init__.py
@@ -2,6 +2,6 @@
 def set_globals(**settings):
 
     global REST_URL
-    REST_URL = settings['rest_url']
-
+    #REST_URL = settings['rest_url']
+    REST_URL = os.environ.get('DATABASE_URL', None);
 
