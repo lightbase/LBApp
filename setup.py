c11a1edf5ce581dda7ddb83483e485c84f570446
Adley Damaceno <adleydamaceno@gmail.com>
2015-05-07 10:41:22 -0300

    Adição de versões para as bibliotecas necessárias

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
-    'pyramid',
-    'pyramid_chameleon',
-    'waitress',
-    'requests'
+    'pyramid == 1.6a1',
+    'pyramid_chameleon == 0.3',
+    'waitress == 0.8.9',
+    'requests == 2.7.0'
     ]
 
 setup(name='LBApp',
