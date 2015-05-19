68613218bb8537a83509cb35a646f70d0ec70274
Alessandro Landim <alessandro.landim@gmail.com>
2015-05-19 13:00:36 -0300

    nova versao app

--- lbapp/config/routes/user.py
+++ lbapp/config/routes/user.py
@@ -4,14 +4,14 @@ from lbapp.views.user import UserView
 
 def make_user_routes(config):
     config.add_route('login', 'login', factory=UserFactory)
-    config.add_view(route_name='login', request_method='GET', renderer='templates/user/login.pt')
+    config.add_view(route_name='login', request_method='GET', renderer='templates/routes/login.pt')
     config.add_view(view=UserView, attr='login', route_name='login', request_method='POST')
 
     config.add_route('logout', 'logout', factory=UserFactory)
     config.add_view(view=UserView, attr='logout', route_name='logout', request_method='POST')
 
     config.add_route('register', 'register', factory=UserFactory)
-    config.add_view(route_name='register', request_method='GET', renderer='templates/user/register.pt')
+    config.add_view(route_name='register', request_method='GET', renderer='templates/routes/register.pt')
     config.add_view(view=UserView, attr='register', route_name='register', request_method='POST')
 
     config.add_route('profile', 'profile/{user}', factory=UserFactory)
--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
-    'pyramid',
-    'pyramid_chameleon',
-    'waitress',
-    'requests'
+    'Chameleon == 2.16',
+    'pyramid == 1.5.1',
+    'pyramid_chameleon == 0.1',
+    'requests == 2.3.0',
+    'waitress == 0.8.9'
     ]
 
 setup(name='LBApp',
