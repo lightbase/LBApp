1d2b50d96f36b34d557df5cbcd9ae13fb8bc44fe
Camilo Souza <camilocarlos@gmail.com>
2015-05-27 10:50:19 -0300

    Inclusão do parâmetro para configurar autenticação LBApp

--- lbapp/config/__init__.py
+++ lbapp/config/__init__.py
@@ -1,7 +1,16 @@
 
+import os
+
 def set_globals(**settings):
 
     global REST_URL
     REST_URL = settings['rest_url']
 
+    global AUTH_ENABLED
+    AUTH_ENABLED = os.environ.get('AUTH_ENABLED', None)
+    if AUTH_ENABLED is None:
+        AUTH_ENABLED = bool(int(settings['auth.enabled']))
+    else:
+        AUTH_ENABLED = bool(int(AUTH_ENABLED))
 
+    print("Configurado : " + str(AUTH_ENABLED))
